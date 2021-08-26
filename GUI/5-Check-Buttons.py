a7abd666731d14ff521f57d3936cfa5859afdc0c
ubuntu-server <patpythonkali@gmail.com>
2021-08-27 00:31:22 +0800

    Updates

--- GUI/5-Check-Buttons.py
+++ GUI/5-Check-Buttons.py
@@ -1,6 +1,26 @@
 from tkinter import *
 
+def display():
+    if(x.get()==1):
+        print("You agree!")
+    else:
+        print("You don't agree")
+
 window = Tk()
-window.title("Check Button")
+window.title('Check Button')
+
+x = BooleanVar()
+
+check_button = Checkbutton(window,
+                           text="I agree to the agreement.",
+                           variable=x,
+                           onvalue=1,
+                           offvalue=0,
+                           command=display,
+                           font=("Ubuntu Mono", 12),
+                           pady=10,
+
+                           )
+check_button.pack()
 
 window.mainloop()
